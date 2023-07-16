898479032ced34af809cd20e7ec3cc7de3117202
Dyn2012 <132987963+Dyn2012@users.noreply.github.com>
2023-07-16 19:31:22 +0700

    Add files via upload

---
+++ ProductListClientController.java
@@ -0,0 +1,32 @@
+package com.thanhhc.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.thanhhc.model.Product;
+import com.thanhhc.service.CategoryService;
+import com.thanhhc.service.ProductService;
+import com.thanhhc.service.impl.CategoryServiceImpl;
+import com.thanhhc.service.impl.ProductServiceImpl;
+
+@WebServlet(urlPatterns = { "/product/list" })
+public class ProductListClientController extends HttpServlet {
+	ProductService productService = new ProductServiceImpl();
+	CategoryService cateService = new CategoryServiceImpl();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Product> productList = productService.getAll();
+		req.setAttribute("productList", productList);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/client/view/product-list.jsp");
+		dispatcher.forward(req, resp);
+	}// cái này sai
+
+}
